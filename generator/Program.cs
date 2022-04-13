ec611e966c10674cb607049fc5e0497ddcef3d95
r.portalez <r.portalez@qwant.com>
2022-04-13 15:41:10 +0200

    move to dotnet 6

--- generator/Program.cs
+++ generator/Program.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace generator
-{
-    class Program
-    {
+namespace generator;
 
-        const string page_template = @"\begin{{landscape}}
+class Program
+{
+    const string page_template = @"\begin{{landscape}}
     \vspace*{{\fill}}
     \begin{{center}}
         {{\fontsize{{40}}{{50}}\selectfont 
@@ -21,7 +20,7 @@ namespace generator
 \end{{landscape}}
 \newpage";
 
-        static readonly string[] mesures = new string[] {
+    const string[] mesures = new string[] {
             "l'instauration de la règle verte",
             "le 100\\% renouvelable",
             "la cantine bio et gratuite",
@@ -42,34 +41,33 @@ namespace generator
 
         };
 
-        static readonly (int year, string[] firstnames)[] buckets = new (int year, string[] firstnames)[] {
+    const (int year, string[] firstnames)[] buckets = new (int year, string[] firstnames)[] {
             (1994, new string[] { "Léa", "Manon", "Camille", "Marie", "Chloé", "Thomas", "Alexandre", "Maxime", "Lucas", "Nicolas"}),
             (1984, new string[] { "Elodie", "Julie", "Aurélie", "Marie", "Emilie", "Julien", "Nicolas", "Kevin", "Thomas", "Alexandre"}),
             (1974, new string[] { "Stéphanie", "Céline", "Virginie", "Sandrine", "Aurélie", "Sébastien", "Nicolas", "David", "Christophe", "Julien"})
         };
 
-        static Random random = new Random();
+    static Random random = new Random();
 
-        static (String name, int age) GetPerson()
-        {
-            var bucket = buckets[random.Next(3)];
-            var year = bucket.year;
-            var age = 2022 - (year + random.Next(10));
-            var firstname = bucket.firstnames[random.Next(10)];
-            return (firstname, age);
-        }
+    static (String name, int age) GetPerson()
+    {
+        var bucket = buckets[random.Next(3)];
+        var year = bucket.year;
+        var age = 2022 - (year + random.Next(10));
+        var firstname = bucket.firstnames[random.Next(10)];
+        return (firstname, age);
+    }
 
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        using (StreamWriter writer = new StreamWriter("../tex/pages.tex"))
         {
-            using (StreamWriter writer = new StreamWriter("../tex/pages.tex"))
+            for (int i = 0; i < 100; ++i)
             {
-                for (int i = 0; i < 100; ++i)
-                {
-                    var person = GetPerson();
-                    var mesure = mesures[random.Next(mesures.Length)];
-                    string page = String.Format(page_template, person.name, person.age, mesure);
-                    writer.WriteLine(page);
-                }
+                var person = GetPerson();
+                var mesure = mesures[random.Next(mesures.Length)];
+                string page = String.Format(page_template, person.name, person.age, mesure);
+                writer.WriteLine(page);
             }
         }
     }
